a826e50bb25d78a3cd8d58001fb8c69116302e6c
西草昌市 <devg1120@gmail.com>
2026-03-10 09:21:16 +0900

    up

--- test_surrealdb_relate1/pytools/basic2.py
+++ test_surrealdb_relate1/pytools/basic2.py
@@ -9,6 +9,10 @@ db = Surreal("ws://localhost:8000")
 db.use("gusa", "dbtest")
 db.signin({"username": "root", "password": "root"})
 
+def pp(str):
+   #pprint.pprint(str)
+   pprint.pprint(str, indent = 2, width = 8)
+
 def i(surql):
    result = db.query(surql)
    print(">> " +  surql )
@@ -20,23 +24,22 @@ def h(text):
    print("")
 
 def table_shema():
-   print("___ TABLE INFO ___")
    print("")
+   print("\033[36m" + "--- TABLE INFO ---" + "\033[0m")
    db_info = db.query("INFO FOR DB;")
    #pprint.pprint(dbinfo)
    pprint.pprint(db_info['tables'])
    for key in db_info['tables']:
-       print('')
+       #print('')
        #print(key)
        print("\033[35m" + key + "\033[0m")
        table_info = db.query("INFO FOR TABLE " + key )
-       pprint.pprint(table_info)
-   print("_________________")
-   print("")
+       #pprint.pprint(table_info)
+       pp(table_info)
 
 def table_delete():
-   print("___ TABLE DELETE ___")
    print("")
+   print("\033[36m" + "--- TABLE DELETE ---" + "\033[0m")
    db_info = db.query("INFO FOR DB;")
    #pprint.pprint(dbinfo)
    #pprint.pprint(db_info['tables'])
@@ -46,13 +49,11 @@ def table_delete():
        print("\033[35m" + key + "\033[0m")
        table_info = db.query("REMOVE TABLE " + key )
        #pprint.pprint(table_info)
-   print("_________________")
-   print("")
 
 def q(surql):
    result = db.query(surql)
    print("")
-   print(">> " + "\033[33m" + surql + "\033[0m")
+   print(">> " + "\033[32m" + surql + "\033[0m")
    return result
 
 def p(surql):
@@ -60,7 +61,8 @@ def p(surql):
    print("")
    print(">> " + "\033[32m" + surql + "\033[0m")
 
-   pprint.pprint(result)
+   #pprint.pprint(result)
+   pp(result)
 
 
 table_delete()
@@ -68,32 +70,29 @@ table_delete()
 #table_shema()
 print("-----------------------")
 
+p("CREATE person:aristotle, article:on_sleep_and_sleeplessness;")
+          #TABLE: REÇØRDID
+p("RELATE person:aristotle->wrote->article:on_sleep_and_sleeplessness;")
 
-q("""
-DEFINE TABLE armor SCHEMAFULL;
-DEFINE FIELD resistance ON armor TYPE int;
-
-DEFINE TABLE player SCHEMAFULL;
-DEFINE FIELD strength ON player TYPE int;
-DEFINE FIELD armor ON player TYPE option<record<armor>>;
-
-CREATE armor:leather SET resistance = 3;
-CREATE armor:chainmail SET resistance = 20;
-CREATE armor:platemail SET resistance = 30;
-
-CREATE player:jack SET strength = 22, armor = armor:platemail;
-CREATE player:brian SET strength = 20, armor = armor:leather;
-
-RELATE player:jack -> wants_to_buy -> armor:dragon;
-RELATE player:jack -> wants_to_buy -> armor:platemail;
-""")
+#table_shema()
+p("SELECT * FROM person, article;")
+p("SELECT * FROM wrote;")
 
 print("-----------------------")
-table_shema()
-print("-----------------------")
-p("SELECT name, email, age FROM users;");
-p("SELECT name, content  FROM posts;");
-p("SELECT *  FROM likes;");
+
+q("DEFINE TABLE person2 SCHEMAFULL;")
+q("DEFINE TABLE article2 SCHEMAFULL;")
+
+p("CREATE person2:aristole")
+p("CREATE article2:on_sleep_and_sleeplessness")
+p("CREATE person2:AAA")
+p("CREATE article2:BBB")
+
+p("RELATE person2:aristotle->wrote2:test01->article2:on_sleep_and_sleeplessness;")
+p("RELATE person2:AAA->wrote2:test02->article2:BBB;")
+
+p("SELECT * FROM person2, article2;")
+p("SELECT * FROM wrote2;")
 
 
 
